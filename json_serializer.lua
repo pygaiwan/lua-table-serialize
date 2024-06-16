b6173f106823bc1ce1e9761301227e33a7e002f3
pygaiwan <moise.medici@gmail.com>
2024-06-16 21:11:15 +0100

    Docstring and start of dot notation serialization

--- json_serializer.lua
+++ json_serializer.lua
@@ -1,9 +1,17 @@
 local json_serializer = {}
 
+---Format basic values.
+---@param value any
+---@return string
 local function basic_serialize(value)
     return string.format("%q", value)
 end
 
+---Serialize to JSON compatible format a table.
+---@param o any
+---@param text string  "Starts to empty."
+---@param indent integer "Indentation used, empty to 0."
+---@return string "Value of text after serialization"
 local function to_json(o, text, indent)
     text = text or ''
     indent = indent or 0
@@ -48,5 +56,23 @@ local function to_json(o, text, indent)
     return text
 end
 
+local function pprint(o, name, text, root)
+    text = text or ''
+    root = root or name
+    local t = type(o)
+    if t == "number" or t == "string" or t == "boolean" or t == "nil" then
+        text = text .. root .. ' = ' .. basic_serialize(o) .. '\n'
+    elseif t == "table" then
+        for k, v in pairs(o) do
+            local new_root = root .. '.' .. k 
+            text = pprint(v, k, text, new_root)
+        end
+    else
+        error("cannot serialize a " .. t)
+    end
+    return text
+end
+
 json_serializer.to_json = to_json
+json_serializer.pprint = pprint
 return json_serializer
--- test_json_serliazer.lua
+++ test_json_serliazer.lua
@@ -1,59 +1,105 @@
-my_json = require("json_serializer")
+my_pkg = require("json_serializer")
 
-describe("Simple table elements parsed correctly:\n", function()
-    local _json = require("dkjson")
+describe("Testing of to_json:\n", function()
+    describe("Simple table elements parsed correctly:\n", function()
+        local _json = require("dkjson")
 
-    it("boolean entry", function()
-        local input = { a = true }
-        local expected = [[{
-  "a": true
-}]]
-        assert.are.same(_json.decode(expected), _json.decode(my_json.to_json(input)))
+        it("boolean entry", function()
+            local input = { a = true }
+            local expected = [[{
+      "a": true
+    }]]
+            assert.are.same(_json.decode(expected), _json.decode(my_pkg.to_json(input, '', 0)))
+        end)
+
+        it("string entry", function()
+            local input = { a = "a" }
+            local expected = [[{
+      "a": "a"
+    }]]
+            assert.are.same(_json.decode(expected), _json.decode(my_pkg.to_json(input, '', 0)))
+        end)
+
+        it("list entry", function()
+            local input = { a = { 1, 2, 3 } }
+            local expected = [[{
+      "a": [1, 2, 3]
+    }]]
+            assert.are.same(_json.decode(expected), _json.decode(my_pkg.to_json(input, '', 0)))
+        end)
+
+        it("table entry", function()
+            local input = { a = { b = 1, c = 2, d = 3 } }
+            local expected = [[{
+      "a": {
+        "b": 1,
+        "c": 2,
+        "d": 3
+      }
+    }]]
+            assert.are.same(_json.decode(expected), _json.decode(my_pkg.to_json(input, '', 0)))
+        end)
     end)
 
-    it("string entry", function()
-        local input = { a = "a" }
-        local expected = [[{
-  "a": "a"
-}]]
-        assert.are.same(_json.decode(expected), _json.decode(my_json.to_json(input)))
-    end)
 
-    it("list entry", function()
-        local input = { a = { 1, 2, 3 } }
-        local expected = [[{
-  "a": [1, 2, 3]
-}]]
-        assert.are.same(_json.decode(expected), _json.decode(my_json.to_json(input)))
-    end)
-
-    it("table entry", function()
-        local input = { a = { b = 1, c = 2, d = 3 } }
-        local expected = [[{
-  "a": {
-    "b": 1,
-    "c": 2,
-    "d": 3
-  }
-}]]
-        assert.are.same(_json.decode(expected), _json.decode(my_json.to_json(input)))
+    describe("Nested table elements parsed correctly:\n:", function()
+        local _json = require("dkjson")
+        it("Something", function()
+            local input = { a = { b = { c = 1, c2 = "1a" }, d = 2 } }
+            local expected = [[{
+      "a": {
+        "b": {
+          "c": 1,
+          "c2": "1a"
+        },
+        "d": 2
+      }
+    }]]
+            assert.are.same(_json.decode(expected), _json.decode(my_pkg.to_json(input, '', 0)))
+        end)
     end)
 end)
 
 
-describe("Nested table elements parsed correctly:\n:", function()
-    local _json = require("dkjson")
-    it("Something", function()
-        local input = { a = { b = { c = 1, c2 = "1a" }, d = 2 } }
-        local expected = [[{
-  "a": {
-    "b": {
-      "c": 1,
-      "c2": "1a"
-    },
-    "d": 2
-  }
-}]]
-        assert.are.same(_json.decode(expected), _json.decode(my_json.to_json(input)))
+describe("Testing of pprint:\n", function()
+    describe("Simple table elements parsed correctly:\n", function()
+        local function split_lines(s)
+            local lines = {}
+            for line in s:gmatch("([^\n]*)\n?") do
+                if line ~= "" then
+                    table.insert(lines, line)
+                end
+            end
+            return lines
+        end
+        it("boolean entry", function()
+            local input = { a = 1 }
+            local expected = [[input.a = 1
+]]
+
+            assert.are.same(my_pkg.pprint(input, "input"), expected)
+        end)
+        it("string entry", function()
+            local input = { a = "a" }
+            local expected = [[input.a = "a"
+]]
+
+            assert.are.same(my_pkg.pprint(input, "input"), expected)
+        end)
+        it("table entry", function()
+            local input = { a = { b = 1, c = 2, d = 3 } }
+            local expected = [[input.a.b = 1
+input.a.c = 2
+input.a.d = 3
+]]
+            local result = my_pkg.pprint(input, "input")
+            local result_lines = split_lines(result)
+            local expected_lines = split_lines(expected)
+
+            table.sort(result_lines)
+            table.sort(expected_lines)
+
+            assert.are.same(result_lines, expected_lines)
+        end)
     end)
 end)
